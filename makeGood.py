70aea776c7274b9ccacab4017a9c67e55bddabe2
Yeswanth-Sikha <sikhayeswanthkumar@gmail.com>
2022-04-11 23:39:29 +0530

    added makeGood.py

---
+++ makeGood.py
@@ -0,0 +1,28 @@
+def makeGood(s: str) -> str:
+    chars = list(s)
+    change = 1
+    while change > 0:
+        change = 0
+        for i in range(len(chars[:-1])):
+            # check for Lower, Upper
+            if (chars[i].islower()):
+                if(chars[i].upper() == chars[i+1]):
+                    chars.pop(i)
+                    chars.pop(i)
+                    change = 1
+                    break
+            # check for Upper, Lower
+            else:
+                if(chars[i].lower() == chars[i+1]):
+                    chars.pop(i)
+                    chars.pop(i)
+                    change = 1
+                    break
+
+    s = ''.join(chars)
+    return s
+
+
+if __name__ == '__main__':
+    s = input()
+    print(makeGood(s))
