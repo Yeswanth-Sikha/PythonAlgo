87e668586328f1120f5489e33af2b8de76830e4c
Yeswanth-Sikha <sikhayeswanthkumar@gmail.com>
2022-04-14 00:11:02 +0530

    added LongestCommonPrefix.py

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,17 @@
+def longestCommonPrefix(strs: list[str]) -> str:
+    reference = strs[0]
+    result = ''
+    for i in range(len(reference)):
+        for string in strs:
+            try:
+                if (reference[i] != string[i]):
+                    return result
+            except:
+                return result
+        result += reference[i]
+    return result
+
+
+if __name__ == "__main__":
+    strs = ["flower", "flow", "flight"]
+    print(longestCommonPrefix(strs))
--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This Repo contains few solved questions from leetcode in python language.
 [5. Jump Search](JumpSearch.py)
 [6. Count Number of Pairs With Absolute Difference K](countKDifference.py)    
 [7. Make The String Great](makeGood.py)
+[8. Length of Last Word](LengthOfLastWord.py)  
+[9. Longest common prefix](LongestCommonPrefix.py)
